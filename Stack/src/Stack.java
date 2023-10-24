211f714776402dd6469729a1bce7691178a7dbed
rsilver24 <rsilver24@kentdenver.org>
2023-10-24 11:57:15 -0600

    Initial code organization w/ instance data and constructors

---
+++ Stack/src/Stack.java
@@ -0,0 +1,34 @@
+public class Stack {
+
+    // Instance Data
+
+    private int size;
+    private Node topItem;
+
+    // Constructors
+
+    public Stack(){
+        size = 0;
+    }
+
+    // Methods
+
+    public void push(E element){
+
+    }
+
+    public T pop(){
+
+    }
+
+    public T peek(){
+
+    }
+    public boolean isEmpty(){
+
+    }
+
+    public int size(){
+
+    }
+}
